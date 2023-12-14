de6ad853f0a42dd5737c872047e55367effb47d8
ChillerDragon <chillerdragon@gmail.com>
2023-12-14 17:56:26 +0100

    Keep banner dark

--- dark.js
+++ dark.js
@@ -2,3 +2,5 @@ const link = document.createElement('link')
 link.rel = 'stylesheet'
 link.href = 'style.css'
 document.head.appendChild(link)
+
+document.querySelector('.navbar').style.setProperty('background-color', 'black', 'important')
